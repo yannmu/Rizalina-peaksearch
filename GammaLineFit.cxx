6248b059fc5e22285e2c2432734bbf119259b15b
Rizalina Mingazheva <rizalina@farm-ui.physik.uzh.ch>
2019-03-08 20:06:47 +0100

    changed the prelim linear fit

--- GammaLineFit.cxx
+++ GammaLineFit.cxx
@@ -32,7 +32,7 @@ namespace std {
         delete res;
     };
 
-    void GammaLineFit::ResetFit(TString name)
+    bool GammaLineFit::ResetFit(TString name, bool ifgammas)
     {
 		  //! create fit function
         if(fFitSet)  delete fFitFunction;
@@ -119,7 +119,16 @@ namespace std {
         double s2 = 0.;
         double f2 = 0.;
         if(fPeakPos.size() == 1){
-			TF1 *f2 = new TF1("linfit", "[0]+[1]*x", fRange.first, fRange.second);
+			double x1, x2 ;
+			if (fPeakPos.at(0) - fRange.first >= fRange.second - fRange.second ){
+				x1 = fRange.first;
+				x2= fPeakPos.at(0) - 5;
+			}
+			else{
+				x1 = fPeakPos.at(0) + 5;
+				x2 = fRange.second;
+			}
+			TF1 *f2 = new TF1("linfit", "[0]+[1]*x", x1, x2);
 			fHist->Fit("linfit", "FLR");
 			
 			f = f2->GetParameter(0);
@@ -129,23 +138,31 @@ namespace std {
 
 		}
 		else{
-			TF1 *fl1 = new TF1("linfit1", "[0]+[1]*x", fRange.first, fPeakPos.at(1)-3);
-			TF1 *fl2 = new TF1("linfit2", "[0]+[1]*x", fPeakPos.at(1)+3, fRange.second );
+			double max_peak = *max_element(fPeakPos.begin(), fPeakPos.end());
+			double min_peak = *min_element(fPeakPos.begin(), fPeakPos.end());
+			std::cout<<"Max/min value: "<<max_peak << "\t"<< min_peak << std::endl;
+			
+			
+			TF1 *fl1;// = new TF1();
+			TF1 *fl2;// = new TF1();
+			
 			double cndf1 = 0.;
 			double cndf2 = 0.;
 			
-			if( fPeakPos.at(1)- fRange.first > 4){
+			if( min_peak- fRange.first > 8){
+				fl1 = new TF1("linfit1", "[0]+[1]*x", fRange.first,min_peak-4);
 				std::cout << "*** ******************* 1st fit **********************" <<  std::endl;
-				fHist->Fit(fl1, "LRF");
+				fHist->Fit(fl1, "LR");
 				cndf1 = double(fl1->GetChisquare()/fl1->GetNDF());
 				std::cout << "Goodnes 1st lin" <<fl1->GetNDF()<<"\t" << cndf1 << std::endl;
 				f1 = (fl1->GetParameter(0));
 	            s1 = (fl1->GetParameter(1));
 			}else std::cout << "1st peak is out of range" << std::endl;
 			
-			if (fRange.second - fPeakPos.at(1) > 4){
+			if (fRange.second - max_peak > 8){
+				fl2 = new TF1("linfit2", "[0]+[1]*x", max_peak+4, fRange.second );
 				std::cout << "*********************** 2nd fit **********************" <<  std::endl;
-                fHist->Fit(fl2, "LRF+");
+                fHist->Fit(fl2, "LRF");
                 cndf2 = double(fl2->GetChisquare()/fl2->GetNDF());
 				std::cout << "Goodnes 2nd lin"  << fl2->GetNDF()<<"\t" <<cndf2 << std::endl;
 				 f2 = (fl2->GetParameter(0));
@@ -157,8 +174,16 @@ namespace std {
 			 if (cndf1 > 1.1 || cndf1 < 0.5){
 				 
 				 if (cndf2 > 1.1 || cndf2 < 0.5){
-					std::cout << "both fits are screwed" << std::endl;
-					exit(0);
+					 
+					std::cout << "***********************************" << std::endl;
+					
+					std::cout << "EROOR:both fits are screwed" << std::endl;
+					
+					std::cout << "***********************************" << std::endl;
+					
+					//exit(0);
+					
+					return false;
 					f = maxBkg;
 					s = 0;				 
 				 }
@@ -176,9 +201,8 @@ namespace std {
 				f = (f1+f2)/2.;		
 				s = (s1+s2)/2.;			
 			}
-			delete fl1;
-			delete fl2;
-			}
+			
+		}
 		slope = s;
 		free_param = f;
 		if (slope < 0)	{			
@@ -187,13 +211,18 @@ namespace std {
 		else {
 			fFitFunction->SetParLimits(fPeakPos.size()*3+1, 0.6*slope, 1.4*slope);		
 		}
-		
-		if (free_param < 0)	{			
+		/*
+		if (free_param < 0)	{
+			free_param = maxBkg;			
 			fFitFunction->SetParLimits(fPeakPos.size()*3, 1.4*free_param, 0.6*free_param);
 		}
 		else {
 			fFitFunction->SetParLimits(fPeakPos.size()*3, 0.6*free_param, 1.4*free_param);
-		}
+		}*/
+		if (free_param < 0)
+			free_param = maxBkg;			
+			fFitFunction->SetParLimits(fPeakPos.size()*3, 0.6*free_param, 1.4*free_param);
+		
 		
         std::cout << "Slope: " << slope << std::endl;
 		std::cout << "free term: " << free_param << std::endl;
@@ -215,14 +244,39 @@ namespace std {
                 fPeakPriors.pop_back();
             }
         }
-        for(int i=0; i<(int) fPeakPos.size(); i++) {
-            if(fResolPrior){
-                
-                fHistFitter->GetParameter(Form("fwhm%d",i))->Fix(fRes->Eval(fPeakPos.at(i)));
-            }
-            fHistFitter->GetParameter(Form("mean%d",i))->Fix(fPeakPos.at(i));
-        }
-        //! set options for MC
+        
+        
+       /* if(ifgammas){
+			std::cout << "setting priors" << std::endl;
+			for(int i=0; i<fNPeaks; i++) {      
+				fFitFunction->SetParLimits(3*i+0, fPeakPos.at(i)-kPeakPosRange,
+											fPeakPos.at(i)+kPeakPosRange); 
+				//fFitFunction->SetParLimits(3*i+1, fRes->Eval(fPeakPos.at(i))-kFWHMRange,
+					//						fRes->Eval(fPeakPos.at(i))+kFWHMRange);
+				fFitFunction->SetParLimits(3*i+2, 0.0, maxPeakCounts.at(i));
+			}
+			for(int i=0; i<(int) fPeakPos.size(); i++) {
+				
+				fPeakPriors.push_back(new TF1("peak","gaus(0)",0.,3000.));
+				fPeakPriors.back()->SetParameters(1,fPeakPos.at(i),fPeakPriorWidth);
+				fHistFitter->SetPrior(Form("mean%d",i),fPeakPriors.back());
+				
+				fHistFitter->GetParameter(Form("fwhm%d",i))->Fix(fRes->Eval(fPeakPos.at(i)));				
+			}
+		}*/
+        
+		std::cout << "paramters fixed to" << std::endl;
+		for(int i=0; i<(int) fPeakPos.size(); i++) {			
+			std::cout << "mean" << i << "\t" << fPeakPos.at(i) << std::endl;
+			fHistFitter->GetParameter(Form("mean%d",i))->Fix(fPeakPos.at(i));
+			if(fResolPrior){
+	
+				fHistFitter->GetParameter(Form("fwhm%d",i))->Fix(fRes->Eval(fPeakPos.at(i)));
+			}
+			std::cout << "fwhm" << i << "\t" << fRes->Eval(fPeakPos.at(i)) << std::endl;
+		}
+	
+		//! set options for MC
         fHistFitter->SetOptimizationMethod(BCModel::kOptMinuit);
         fHistFitter->MCMCSetPrecision(fPrecision);
         for(int i=0; i<(int) fPeakPos.size(); i++) {
@@ -232,22 +286,25 @@ namespace std {
         }
         fHistFitter->GetParameter("bkgrnd")->SetNbins(5000);
         fHistFitter->GetParameter("slpe")->SetNbins(5000);
+
+
+        return true;
     }
 
-    void GammaLineFit::Fit(TString name)
+    bool GammaLineFit::Fit(TString name, bool ifgammas)
     {
         //! check if all ingredient available
         if(! fHistSet) {
             cerr << "error: no histogram set" << endl;
-            return;
+            return false;
         }
         if(! fResSet) {
             cerr << "error: no resolution set" << endl;
-            return;
+            return false;
         }
         if(! fRangeSet) {
             cerr << "error: no range set" << endl;
-            return;
+            return false;
         }
 		
         //! open bc log file
@@ -257,22 +314,25 @@ namespace std {
         std::cout << "BCAux::SetStyle();" << std::endl;
 		
         //! reset fit
-        ResetFit(name);
-		std::cout << "ResetFit(name);"<< std::endl;
-	
-        //! perform fit
-       fHistFitter->Fit();
+        if(ResetFit(name, ifgammas)){
+			std::cout << "ResetFit(name);"<< std::endl;
+		
+			//! perform fit
+			
+		   fHistFitter->Fit();
 			std::cout << "       fHistFitter->Fit();"<< std::endl;
 
-        // evaluate fit
-        EvaluateFit(name);
-        		std::cout << "        EvaluateFit(name);"<< std::endl;
+			// evaluate fit
+			EvaluateFit(name);
+			std::cout << "        EvaluateFit(name);"<< std::endl;
 
-       // close log file
-		BCLog::CloseLog();
+		   // close log file
+			BCLog::CloseLog();
 
-        fFitSet = true;
-       
+			fFitSet = true;
+			return true;
+		}
+		return false;
     }
     
    /*
@@ -444,54 +504,56 @@ fHistFitter->GetFitFunctionGraph(fHistFitter->GetBestFitParameters())->GetPoint(
         fCanvas2->Clear();
         fCanvas2->cd();
 		
-		/*std::cout << "check marginalised" << std::endl;
-		vector<string> marhists(5);
+		int nparams = 3;
+		vector<string> marhists(nparams);
 		marhists[0] = "intnsty0";
 		marhists[1] = "bkgrnd";
-		marhists[2] = "mean0";
-		marhists[3] = "fwhm0";
-		marhists[4] = "slpe";
-
-		ofstream outfile;
-		char fname [50];
-		//sprintf("%s.txt", name);
-		//outfile.open(name.Data());
+		//marhists[3] = "fwhm0";
+		marhists[2] = "slpe";
+		
+		char fname [500];
+		sprintf(fname, "%s_MCMC.txt", std::string(name).c_str());
+		
+		std::cout << fname << std::endl;
+		std::ofstream outfile(fname);
+		outfile << "*******************************************" << endl;
 		std::cout << "*******************************************" << endl;
-		for (int npar  = 0; npar<4;npar++){
-			std::cout << marhists[npar] << endl;
+		for (int npar  = 0; npar<nparams;npar++){
+			outfile << marhists[npar] << endl;
+			outfile << marhists[npar] << endl;
 			BCH1D* mar = fHistFitter->GetMarginalized(Form(marhists[npar].c_str()));
 			double mode = mar->GetMode();
 			double quantile90 = mar->GetQuantile(0.9);
-			std::cout << "mode:" << "\t" << mode << endl;
-			std::cout << "mean:" << "\t" << mar->GetMean() << endl;
-			std::cout << "STD:" << "\t" << mar->GetSTD() << endl;
+			outfile << "mode:" << "\t" << mode << endl;
+			outfile << "mean:" << "\t" << mar->GetMean() << endl;
+			outfile << "STD:" << "\t" << mar->GetSTD() << endl;
 			double low, high;
 			mar->GetSmallestInterval(low,high);
-			std::cout << "68% smallest interval:" << "\t" << "["<<
+			outfile << "68% smallest interval:" << "\t" << "["<<
 					low << ";" << high << "]" << endl;
 
 			mar->GetSmallestInterval(low,high,0.955);
-			std::cout << "95% smallest interval:" << "\t" << "["<<
+			outfile << "95% smallest interval:" << "\t" << "["<<
 					low << ";" << high << "]" << endl;
 
 
 			mar->GetSmallestInterval(low,high, 0.997);
-			std::cout << "97% smallest interval:" << "\t" << "["<<
+			outfile << "97% smallest interval:" << "\t" << "["<<
 					low << ";" << high << "]" << endl;
 
-			std::cout <<"bin center array" <<endl;
+			outfile <<"bin center array" <<endl;
 			TH1D * h_mar = mar->GetHistogram();
 			int nentries = h_mar->GetEntries();
-			/*for(int i =0; i<nentries; i++){
-				std::cout << h_mar->GetBinCenter(i) << "\t";
-			}
-			std::cout <<endl;
 			for(int i =0; i<nentries; i++){
-				std::cout << h_mar->GetBinContent(i) << "\t";
+				outfile << h_mar->GetBinCenter(i) << "\t";
 			}
-			std::cout<<endl;
+			outfile <<endl;
+			for(int i =0; i<nentries; i++){
+				outfile << h_mar->GetBinContent(i) << "\t";
+			}
+			outfile<<endl;
 
-		}*/
+		}
         //! png output of fit result
         
         fHistFitter->DrawFit("HIST", false); // draw without(?) a legend
@@ -500,7 +562,7 @@ fHistFitter->GetFitFunctionGraph(fHistFitter->GetBestFitParameters())->GetPoint(
         //fCanvas2->Print(Form("%s.root",name.Data()));
     }
 
-    void GammaLineFit::WriteToCommonLog(TString name, TString logFileName, double exposure)
+    void GammaLineFit::WriteToCommonLog(TString name, TString logFileName, double exposure, bool ifgammas )
     {
         if(! fFitSet) {
             cout << "no fit performed, nothing to write to log" << endl;
@@ -532,23 +594,34 @@ fHistFitter->GetFitFunctionGraph(fHistFitter->GetBestFitParameters())->GetPoint(
                  << endl;
 
         for(int i=0;i<fNPeaks;i++) {
+			
+			if(ifgammas){
+							   xlog << "-----------------------------------------------------" << endl
+					<< "peakPos:   "
+					<< setw(7)  << setprecision(2) << fixed << fPeakPos.at(i) << " -> "
+					<< setw(7)  << setprecision(2) << fixed
+					<< Form("%.2f",fHistFitter->GetBestFitParameter(3*i+0))  << " +- "
+					<< setw(4)  << setprecision(2) << fixed
+					<< Form("%.2f",fHistFitter->GetBestFitParameterError(3*i+0)) << " keV"
+					<< endl
+					<< "FWHM:      "
+					<< setw(7)  << setprecision(2) << fixed << fRes->Eval(fPeakPos.at(i)) << " -> "
+					<< setw(7)  << setprecision(2) << fixed
+					<< Form("%.2f",fHistFitter->GetBestFitParameter(3*i+1))  << " +- "
+					<< setw(4)  << setprecision(2) << fixed
+					<< Form("%.2f",fHistFitter->GetBestFitParameterError(3*i+1)) << " keV"
+					<< endl;
+			}
 			//Since peak pos and fwhm are fixed
-				/*xlog << "-----------------------------------------------------" << endl
+			else{
+				xlog << "-----------------------------------------------------" << endl
                  << "peakPos:   "
-                 << setw(7)  << setprecision(2) << fixed << fPeakPos.at(i) << " -> "
-                 << setw(7)  << setprecision(2) << fixed
-                 << Form("%.2f",fHistFitter->GetBestFitParameter(3*i+0))  << " +- "
-                 << setw(4)  << setprecision(2) << fixed
-                 << Form("%.2f",fHistFitter->GetBestFitParameterError(3*i+0)) << " keV"
+                 << setw(7)  << setprecision(2) << fixed << fPeakPos.at(i)
                  << endl
                  << "FWHM:      "
-                 << setw(7)  << setprecision(2) << fixed << fRes->Eval(fPeakPos.at(i)) << " -> "
-                 << setw(7)  << setprecision(2) << fixed
-                 << Form("%.2f",fHistFitter->GetBestFitParameter(3*i+1))  << " +- "
-                 << setw(4)  << setprecision(2) << fixed
-                 << Form("%.2f",fHistFitter->GetBestFitParameterError(3*i+1)) << " keV"
+                 << setw(7)  << setprecision(2) << fixed << fRes->Eval(fPeakPos.at(i))           
                  << endl;
-*/
+			}
 
 			BCH1D* mar_bkg = fHistFitter->GetMarginalized(Form("bkgrnd"));
 			TH1D * hbkg = mar_bkg->GetHistogram();
